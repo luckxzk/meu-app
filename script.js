e1bd457b1a789849f269bc369871fdc6003bb4f1
luckxzk <luquinhasamuel913@gmail.com>
2025-03-06 22:36:38 -0300

    Adicionando arquivos do projeto

--- script.js
+++ script.js
@@ -68,4 +68,45 @@ formularioTarefa.addEventListener('submit', (e) => {
     formularioTarefa.reset();
 });
 
-atualizarEstrelas();
+atualizarEstrelas();const staticAssets = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './icon-192x192.png',
+    './icon-512x512.png'
+  ];
+  
+  self.addEventListener('install', async event => {
+    const cache = await caches.open('static-assets');
+    await cache.addAll(staticAssets);
+  });
+  
+  self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+  
+    if (url.origin === location.origin) {
+      event.respondWith(cacheFirst(req));
+    } else {
+      event.respondWith(networkFirst(req));
+    }
+  });
+  
+  async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+  }
+  
+  async function networkFirst(req) {
+    const cache = await caches.open('dynamic-assets');
+  
+    try {
+      const res = await fetch(req);
+      cache.put(req, res.clone());
+      return res;
+    } catch (error) {
+      const cachedResponse = await cache.match(req);
+      return cachedResponse || await caches.match('./offline.html');
+    }
+  }
